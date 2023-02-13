7cca72c455a3a2642298930138bb5c31551cd40c
VicRoger27 <121492850+VicRoger27@users.noreply.github.com>
2023-02-13 11:38:20 +0000

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-i1 = 0
 i2 = 0
+i1 = 0
+led.enable(False)
 change1 = 1
 change2 = 1
-while True:
-    pins.analog_write_pin(AnalogPin.P0, i1)
-    pins.analog_write_pin(AnalogPin.P1, 500-i1)
-    pins.analog_write_pin(AnalogPin.P2, i1)
-    pins.analog_write_pin(AnalogPin.P8, 500-i1)
-    pins.analog_write_pin(AnalogPin.P16, i2)
 
-    i1 = i1 + change1
-    if i1 >= 500:
-        change1 = -1
-    if i1 < 0:
-        change1 = 1
-
-    i1 = i1 + change1
-    if i2 >= 1000:
-        change2 = -1
-    if i1 < 0:
-        change2 = 1
-
-
+def on_forever():
+    global i1, change1, i2, change2
+    while True:
+        pins.analog_write_pin(AnalogPin.P0, i1)
+        pins.analog_write_pin(AnalogPin.P1, 500 - i1)
+        #pins.analog_write_pin(AnalogPin.P2, i1)
+        #pins.analog_write_pin(AnalogPin.P8, 500 - i1)
+        pins.analog_write_pin(AnalogPin.P6, i2)
+        i1 = i1 + change1
+        if i1 >= 500:
+            change1 = -1
+        if i1 < 1:
+            change1 = 1
+        i2 = i2 + change2
+        if i2 >= 1000:
+            change2 = -1
+        if i1 < 1:
+            change2 = 1
+        basic.pause(10)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,26 +1,29 @@
+let i2 = 0
 let i1 = 0
+led.enable(false)
 let change1 = 1
 let change2 = 1
-while (true) {
-    let i2 = 0
-    pins.analogWritePin(AnalogPin.P0, i1)
-    pins.analogWritePin(AnalogPin.P1, 500 - i1)
-    pins.analogWritePin(AnalogPin.P2, i1)
-    pins.analogWritePin(AnalogPin.P8, 500 - i1)
-    pins.analogWritePin(AnalogPin.P16, i2)
-    i1 = i1 + change1
-    if (i1 >= 500) {
-        change1 = -1
+basic.forever(function () {
+    while (true) {
+        pins.analogWritePin(AnalogPin.P0, i1)
+        pins.analogWritePin(AnalogPin.P1, 500 - i1)
+        // pins.analog_write_pin(AnalogPin.P2, i1)
+        // pins.analog_write_pin(AnalogPin.P8, 500 - i1)
+        pins.analogWritePin(AnalogPin.P6, i2)
+        i1 = i1 + change1
+        if (i1 >= 500) {
+            change1 = -1
+        }
+        if (i1 < 1) {
+            change1 = 1
+        }
+        i2 = i2 + change2
+        if (i2 >= 1000) {
+            change2 = -1
+        }
+        if (i1 < 1) {
+            change2 = 1
+        }
+        basic.pause(10)
     }
-    if (i1 < 0) {
-        change1 = 1
-    }
-    i1 = i1 + change1
-    if (i2 >= 1000) {
-        change2 = -1
-    }
-    if (i1 < 0) {
-        change2 = 1
-    }
-    basic.pause(100)
-}
+})
